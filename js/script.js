029ce3b57c4ba2287a186823532e3988e2c87902
mugisha12 <58938904+mugisha12@users.noreply.github.com>
2020-07-13 01:37:12 +0200

    13

--- js/script.js
+++ js/script.js
@@ -30,4 +30,40 @@ $(document).ready(function(){
             $('.t-navbar').css("background-color","white");
         }
     }); */
-})
+    $('.t-drv-e-card-hover').css({'height': '46.7px'});
+    $(window).resize(function(){
+        var hoHeight = $('.t-drv-e-card-hover');
+        var infHeight = $('.t-drv-e-card-inf').height();
+        hoHeight.css({'height': infHeight +'px'});
+    });
+        slider = $('#autoWidth').lightSlider({
+        item:5,
+        loop:false,
+        slideMove:1,
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        speed:600,
+        responsive : [
+            {
+                breakpoint:800,
+                settings: {
+                    item:3,
+                    slideMove:1,
+                    slideMargin:15,
+                  }
+            },
+            {
+                breakpoint:480,
+                settings: {
+                    item:2,
+                    slideMove:1
+                  }
+            }
+        ]
+    });  
+    $('.t-drv-e-btn-l').click(function(){
+      slider.goToPrevSlide();
+    });
+    $('.t-drv-e-btn-r').click(function(){
+      slider.goToNextSlide();
+    });
+});
